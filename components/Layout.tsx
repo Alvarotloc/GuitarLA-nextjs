583a841522ffeffd74fc36571a40331b0197a34f
Alvarotloc <alvarotloc@gmail.com>
2022-04-26 18:51:15 +0200

    Header terminado, estilizado y responsive, próximo paso hacer footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Header from './Header';
+
 interface ILayout {
     children : JSX.Element;
     pagina : string;
@@ -10,6 +12,7 @@ const Layout = ({children,pagina}:ILayout):JSX.Element => {
             <title>GuitarLA {pagina}</title>
             <meta name="description" content="Sitio web de venta de guitarras" />
         </Head>
+        <Header />
         {children}
     </div>
   )
